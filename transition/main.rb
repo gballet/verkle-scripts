b60a3a6d6762af8bbd73ed667c4dd9a704bef5f6
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2023-01-09 17:05:27 +0100

    save the transition status into a db

--- transition/main.rb
+++ transition/main.rb
@@ -5,13 +5,42 @@ require 'sinatra'
 require 'json'
 require 'net/http'
 require 'uri'
+require 'sequel'
+require 'optimist'
 
-mpt_url = "https://localhost:8551"
-vkt_url = "https://localhost:8552"
+# Parse the command line arguments
+opts = Optimist::options do
+  opt :fork_block, "Block number at which to fork", type: :integer
+  opt :mpt_url, "URL of the MPT backend", type: :string, default: "https://localhost:8551"
+  opt :vkt_url, "URL of the verkle backend", type: :string, default: "https://localhost:8552"
+end
+
+fork_block = opts[:fork_block]
+mpt_url = opts[:mpt_url]
+vkt_url = opts[:vkt_url]
+
+DB = Sequel.connect('sqlite://transition.db')
+
+# Create the payloads table if it doesn't exist
+DB.create_table? :payloads do
+  primary_key :id
+  String :data
+end
+
+# Create the status table if it doesn't exist
+DB.create_table? :status do
+  primary_key :id
+  Boolean converted
+  Boolean transitionned
+end
+
+status = DB[:status].first
+converted = !status.nil? && status[:converted]
 
 # Block number at which the fork happens
 FORK_BLOCK = 1000
 
+# A helper function to call one of the backends
 def forward_call url, data
   uri = URI(url)
   https = Net::HTTP.new(uri.host, uri.port)
@@ -23,9 +52,6 @@ def forward_call url, data
   https.request(req).body.read
 end
 
-converted = false
-backlog = []
-
 # This implements a post handler, that redirects
 # each RPC call to both the verkle and MPT backends,
 # until the transition block is reached.
@@ -35,16 +61,21 @@ post '/' do
   method = command['method']
   parameters = command['params']
   number = parameters['number'].to_i(16)  
-  transitionned = number < FORK_BLOCK
+  
+  # Check if we reached the transition condition
+  if transitionned == false && number >= fork_block
+    transitionned = true
+    status.update(transitionned: true)
+  end
 
   if converted
     # Also send it to the conversion code, if it
     # is transitioned
-    forward_call(vkt_url, data) if transitionned
+    forward_call(vkt_url, data) unless transitionned
   else
     # Just save the call, to be replayed once the
-    # transition has occured.
-    backlog << parameters
+    # conversion has completed.
+    DB[:payloads].insert(data: parameters)
   end
 
   forward_call(transitionned ? vkt_url : mpt_url, data)
@@ -53,12 +84,14 @@ end
 # Indicate that the data has been converted. It will
 # replay all saved blocks.
 post '/converted' do
-  converted = true
-  
-  backlog.each.with_index do |payload, index|
-    forward_call(vkt_url, payload)
+  DB[:payloads].each do |row|
+    forward_call(vkt_url, row[:payload])
   end
   
+  # Clear the table
+  DB[:payloads].delete
+  
+  # Send a fork choice update message
   forward_call(vkt_url, {
     jsonrpc: "2.0",
     method: "engine_forkchoiceUpdatedV1",
@@ -70,4 +103,7 @@ post '/converted' do
       }
     ]
   }.to_json)
+  
+  converted = true
+  status.update(converted: true)
 end
