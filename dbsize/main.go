3f67061201d98a9bd2e7c1248496c4920bf21e3a
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2024-09-26 12:41:26 +0200

    print out how much space is taken by each node type

--- dbsize/main.go
+++ dbsize/main.go
@@ -18,12 +18,22 @@ func main() {
 	// Variable to store the total size of the values
 	prefix := []byte("flat-")
 	var (
-		totalSize     int
+		totalSize int
+
 		totalInternal int
-		totalBitmap   int
-		totalEoA      int
-		totalSingle   int
+		internalSize  int
+
+		totalBitmap int
+		bitmapSize  int
+
+		totalEoA int
+		eoaSize  int
+
+		totalSingle int
+		singleSize  int
+
 		totalSkipList int
+		skiplistSize  int
 
 		savedSkipList int
 	)
@@ -40,16 +50,21 @@ func main() {
 		switch firstByte {
 		case 1:
 			totalInternal++
+			internalSize += len(iter.Value())
 		case 2:
 			totalBitmap++
+			bitmapSize += len(iter.Value())
 		case 3:
 			totalEoA++
+			eoaSize += len(iter.Value())
 		case 4:
 			totalSingle++
+			singleSize += len(iter.Value())
 		case 8:
 			totalSkipList++
 
 			savedSkipList += 32 - len(iter.Value())%32
+			skiplistSize += len(iter.Value())
 		default:
 			fmt.Println("invalid type", firstByte)
 			panic("invalid type")
@@ -60,5 +75,12 @@ func main() {
 	}
 	iter.Release()
 
-	fmt.Println("found", totalSize, "bytes", "internal", totalInternal, "bitmap", totalBitmap, "EoA", totalEoA, "single slot", totalSingle, "skip list", totalSkipList, "(", savedSkipList, "bytes saved)")
+	fmt.Println("found", totalSize, "bytes, ", savedSkipList, "bytes saved with skiplists")
+	fmt.Printf("%-10s %-15s %-15s\n", "Type", "Count", "Size")
+	fmt.Println("----------------------------------------")
+	fmt.Printf("%-10s %-15d %-15d\n", "Branch", totalInternal, internalSize)
+	fmt.Printf("%-10s %-15d %-15d\n", "Bitmap", totalBitmap, bitmapSize)
+	fmt.Printf("%-10s %-15d %-15d\n", "EoA", totalEoA, eoaSize)
+	fmt.Printf("%-10s %-15d %-15d\n", "Single", totalSingle, singleSize)
+	fmt.Printf("%-10s %-15d %-15d\n", "Skiplist", totalSkipList, skiplistSize)
 }
